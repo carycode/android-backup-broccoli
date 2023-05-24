1cc2701d8b04d800d3a615df2f17ec79ac4db4ae
David Cary <d.cary+2012@ieee.org>
2023-05-24 14:24:03 -0700

    boilerplate new Python script

---
+++ ab.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+"""
+
+
+def main():
+    print("starting ...")
+    print("done!")
+
+if __name__ == "__main__":
+    main()
+
+# as recommended by https://wiki.python.org/moin/Vim :
+# vim: set tabstop=8 expandtab shiftwidth=4 softtabstop=4 :
+
